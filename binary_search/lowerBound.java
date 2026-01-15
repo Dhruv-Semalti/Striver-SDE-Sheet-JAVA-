5e3490cd80bf6b1b9f2540da7b97fee7efdb6ffb
Dhruv Semalti <dhruvsemalti02@gmail.com>
2026-01-16 00:53:13 +0530

    binary_search

---
+++ binary_search/lowerBound.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    public static void main(String args[])
+    {
+        int a[]={1,2,2,2,3};
+        int x=2;
+        System.out.println(lowerBound(a,x));
+    }
+    
+    public static int lowerBound(int[] a, int x) {
+       int len=a.length;
+       int low=0;
+       int high=len-1;
+       int mid=0;
+       int backup=0;
+       while(low<=high)
+       {
+           mid=low+(high-low)/2;
+           if(a[mid]>=x)
+           {
+               ans=mid;
+               high=mid-1;
+           }
+           else
+           {
+               low=mid+1;
+             
+           }
+       }
+       return ans;
+     }
+}
